65abd56fb0831d0fce08ed084c1dc53c88c4bcad
Super User <root@n1.intra.kat.sh>
2025-10-28 12:05:39 +0000

    aaa

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 file=$1
 dest=$2
-uri=$3
-skopeo login
+registry=$3
+uri=$4
+skopeo login $3
 apptainer build --sandbox $dest $file 
 tar cvf $dest.tar $dest/*
-skopeo copy tarball:$dest.tar docker://$uri
+skopeo copy tarball:$dest.tar docker://$registry/$uri
