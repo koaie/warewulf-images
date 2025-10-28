159f4204ee0482461b335cccff6bb6895cae7491
Super User <root@n1.intra.kat.sh>
2025-10-28 12:32:51 +0000

    yes

--- build.sh
+++ build.sh
@@ -4,7 +4,13 @@ file=$1
 dest=$2
 registry=$3
 uri=$4
+
+build_path=/tmp/warewulf/build/
+mkdir -p $buildpath
+
 skopeo login $3
-apptainer build --sandbox $dest $file 
-tar cvf $dest.tar $dest/*
+apptainer build --sandbox $buildpath/$dest $file 
+tar cvf $build_path/$dest.tar build_path/$dest/*
 skopeo copy tarball:$dest.tar docker://$registry/$uri
+rm -rf "/tmp/warewulf/build"
+
